aee022c7d522ef3110ab1082766eeb0898470b1c
Shayla Sexton <sesexton@aggies.ncat.edu>
2020-04-20 20:35:22 -0500

    random num string

---
+++ addapp/addapp/String.swift
@@ -0,0 +1,26 @@
+//
+//  String.swift
+//  addapp
+//
+//  Created by Shayla Sexton on 4/20/20.
+//  Copyright © 2020 Shayla Sexton. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static func randomNumber(length: Int) -> String{
+        
+        let number = String.randomNumber(length: 4)
+        var result = ""
+        for _ in 0..<length {
+            let digit = Int.random(in: 0...9)
+            result += "\(digit)"
+            
+        }
+        
+        return result
+        
+    }
+    
+}
